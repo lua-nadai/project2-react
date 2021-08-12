bf60d9b2b63730f118dcf1217c300d95aac8a65f
Luana <luana.nadai.santos@gmail.com>
2021-08-11 21:00:20 -0300

    API CryptoCoins created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import './App.css';
-import CryptoCard from './components/CryptoCard';
+import React, { Component } from "react";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Oi</h1>
-    </div>
-  );
+import './App.css';
+
+import CryptoCard from './components/CryptoCard';
+import ApiCrypto from './api/api.js'
+
+
+class App extends Component {
+
+  state= {
+    cryptoList: [],
+  }
+
+  componentDidMount() {
+    ApiCrypto.listCoins('usd').then((result) => {
+      this.setState({
+        cryptoList: result.data
+      })
+    }) 
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Oi</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
